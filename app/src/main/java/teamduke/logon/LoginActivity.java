67cad85b0e8f157037a2b429bfb36915e13adada
Luke <LukeEvans093@gmail.com>
2015-04-13 20:31:42 +0100

    First attempt
    Not working as expected. Waiting for review

---
+++ app/src/main/java/teamduke/logon/LoginActivity.java
@@ -0,0 +1,69 @@
+package teamduke.logon;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+public class LoginActivity extends Activity {
+
+    private  EditText username = null;
+    private  EditText password = null;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        EditText username = (EditText) findViewById(R.id.editText1);
+        EditText password = (EditText) findViewById(R.id.editText2);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_login, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void login(View view)
+         {
+             Intent getMainActivity = new Intent(this,MainActivity.class);
+
+             if (username.getText().toString().equals("255051") &&
+                password.getText().toString().equals("password"))
+
+             {
+
+                 final int result = 1;
+                 getMainActivity.putExtra("callingActivity", "MainActivity");
+                 startActivityForResult(getMainActivity, result);
+             }
+
+    }
+}
